d7e3ffe1266e03c5f1648005b2a6c746f0d66336
RafDiaSas <82507228+RafDiaSas@users.noreply.github.com>
2022-06-03 10:48:26 +0100

    1

---
+++ app/src/main/java/com/example/projetofinalpa/Funcionario.kt
@@ -0,0 +1,21 @@
+package com.example.projetofinalpa
+
+import android.content.ContentValues
+
+data class Funcionario(
+    var id: Long,
+    var nome : String,
+    var comissao: Long,
+    var veiculos_vendidos: Long,
+    var veiculos_vendidos: Long
+    ) {
+        fun toContentValues() : ContentValues {
+            val valores = ContentValues()
+
+            valores.put(TabelaBDFuncionarios.CAMPO_NOME, nome)
+            valores.put(TabelaBDFuncionarios.CAMPO_COMISSAO, comissao)
+            valores.put(TabelaBDFuncionarios.CAMPO_VEICULOS_VENDIDOS, veiculos_vendidos)
+
+            return valores
+        }
+}
